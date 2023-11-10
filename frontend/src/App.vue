f10c59ad4c2a1a26c05cb12cbea6c9cc2da26b81
Kirill Korolev <102628920+kkorolevdev@users.noreply.github.com>
2023-11-10 22:52:00 +0300

    feat: add normalize.css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,23 +15,19 @@ fetch('https://django-api-eodw.onrender.com/api/items/?format=json')
 </script>
 
 <template>
-    <div>
-        green
+    <div class="container">
+        <h1>Hello world</h1>
 
-        <p>purple</p>
+        <p>In a land far, far away, where pixels danced and code waltzed, Byte 27 emerged as a beacon of digital creativity. The binary winds whispered secrets of innovation, and the circuitry hummed with the melody of progress. In this enchanted realm, ones and zeros pirouetted in a dazzling display, weaving tales of bytes and dreams. Lorem Ipsum bowed to the Byte 27 symphony, for in this kingdom of creation, the code became poetry, and innovation, an eternal dance.</p>
 
-        {{ data }}
+        <pre>
+            {{ data }}
+        </pre>
+        
     </div>
 </template>
 
 <style lang="scss">
-@import url('./ui/styles.scss');
-
-div {
-    color: palegreen;
-
-    p {
-        color: rebeccapurple;
-    }
-}
+    @import url('./ui/normolize.css');
+    @import url('./ui/styles.scss');
 </style>
