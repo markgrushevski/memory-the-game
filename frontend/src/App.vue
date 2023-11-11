a5c16a73510f8b89f1d5f07573c174e8dcdf4035
Leonid <68914627+markgrushevski@users.noreply.github.com>
2023-11-11 12:59:21 +0500

    some fixes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,14 @@
 <script setup>
+import { useFetch } from '@vueuse/core';
 import { ref } from 'vue';
 
-const data = ref('no data');
+const _data = ref('no data');
 
-fetch('https://django-api-eodw.onrender.com/api/items/?format=json')
-    .then((res) => res.json())
-    .then((res) => {
-        console.log(res);
-        data.value = res;
-    })
-    .catch((e) => {
-        console.error(e);
+useFetch('https://django-api-eodw.onrender.com/api/items/?format=json')
+    .get()
+    .json()
+    .then(({ data }) => {
+        _data.value = data.value;
     });
 </script>
 
@@ -18,16 +16,20 @@ fetch('https://django-api-eodw.onrender.com/api/items/?format=json')
     <div class="container">
         <h1>Hello world</h1>
 
-        <p>In a land far, far away, where pixels danced and code waltzed, Byte 27 emerged as a beacon of digital creativity. The binary winds whispered secrets of innovation, and the circuitry hummed with the melody of progress. In this enchanted realm, ones and zeros pirouetted in a dazzling display, weaving tales of bytes and dreams. Lorem Ipsum bowed to the Byte 27 symphony, for in this kingdom of creation, the code became poetry, and innovation, an eternal dance.</p>
+        <p>
+            In a land far, far away, where pixels danced and code waltzed, Byte 27 emerged as a beacon of digital
+            creativity. The binary winds whispered secrets of innovation, and the circuitry hummed with the melody of
+            progress. In this enchanted realm, ones and zeros pirouetted in a dazzling display, weaving tales of bytes
+            and dreams. Lorem Ipsum bowed to the Byte 27 symphony, for in this kingdom of creation, the code became
+            poetry, and innovation, an eternal dance.
+        </p>
 
         <pre>
-            {{ data }}
+            {{ _data }}
         </pre>
-        
     </div>
 </template>
 
 <style lang="scss">
-    @import url('./ui/normolize.css');
-    @import url('./ui/styles.scss');
+@import url('./ui/styles.scss');
 </style>
