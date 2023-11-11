f780c9d6661b180a27aa210b6e3be9fa468195f4
Kirill Korolev <102628920+kkorolevdev@users.noreply.github.com>
2023-11-11 12:29:29 +0300

    feat: add base game layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,6 +16,93 @@ useFetch('https://django-api-eodw.onrender.com/api/items/?format=json')
     <div class="container">
         <h1>Hello world</h1>
 
+        <section class="field">
+            <div class="field-wrapper">
+                <div class="field-container field-container-4">
+                    <div class="field-container__card field-container__card-type-1">
+                        <span>
+                            #1
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-2">
+                        <span>
+                            #2
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-3">
+                        <span>
+                            #3
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-4">
+                        <span>
+                            #4
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-5">
+                        <span>
+                            #5
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-6">
+                        <span>
+                            #6
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-7">
+                        <span>
+                            #7
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-8">
+                        <span>
+                            #8
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-1">
+                        <span>
+                            #1
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-2">
+                        <span>
+                            #2
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-3">
+                        <span>
+                            #3
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-4">
+                        <span>
+                            #4
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-5">
+                        <span>
+                            #5
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-6">
+                        <span>
+                            #6
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-7">
+                        <span>
+                            #7
+                        </span>
+                    </div>
+                    <div class="field-container__card field-container__card-type-8">
+                        <span>
+                            #8
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <p>
             In a land far, far away, where pixels danced and code waltzed, Byte 27 emerged as a beacon of digital
             creativity. The binary winds whispered secrets of innovation, and the circuitry hummed with the melody of
