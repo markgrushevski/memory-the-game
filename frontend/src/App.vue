08c1d41d55cadc3db53079fc92b82381f04d05d0
Kirill Korolev <102628920+kkorolevdev@users.noreply.github.com>
2023-11-12 16:57:03 +0300

    feat: add close button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -242,7 +242,7 @@ useFetch('https://django-api-eodw.onrender.com/api/items/2')
                         Попробовать еще раз
                     </button>
             </div>
-            <div class="memo-modal__close">
+            <div class="memo-modal__close" @click="showNewGameModal = false">
                 <svg
                     id="Layer_1"
                     xmlns="http://www.w3.org/2000/svg"
@@ -293,6 +293,20 @@ useFetch('https://django-api-eodw.onrender.com/api/items/2')
                 </ul>
 
                 <h2>Удачи и приятной игры!</h2>
+            </div>
+            <div class="memo-modal__close" @click="showNewGameModal = false">
+                <svg
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    data-name="Layer 1"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                >
+                    <path
+                        d="m16.707,8.707l-3.293,3.293,3.293,3.293-1.414,1.414-3.293-3.293-3.293,3.293-1.414-1.414,3.293-3.293-3.293-3.293,1.414-1.414,3.293,3.293,3.293-3.293,1.414,1.414Zm7.293,3.293c0,6.617-5.383,12-12,12S0,18.617,0,12,5.383,0,12,0s12,5.383,12,12Zm-2,0c0-5.514-4.486-10-10-10S2,6.486,2,12s4.486,10,10,10,10-4.486,10-10Z"
+                    />
+                </svg>
             </div>
         </div>
         <div class="memo-modal__overlay" @click.self="closeAllModals"></div>
